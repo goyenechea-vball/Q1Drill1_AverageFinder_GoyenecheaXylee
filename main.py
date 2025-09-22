3a8a78ba5b8d0070940f060f5a775f2bf7e1bb83
goyenechea-vball <goyenechea.xa@obmontessori.edu.ph>
2025-09-22 16:33:18 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from pyscript import display, document
+
+display()
+def ave(e): #e for event handler
+    num1 = float(document.getElementById("input1").value)
+    num2 = float(document.getElementById("input2").value)
+
+    average = (num1 + num2) / 2
+
+    if average >= 75:
+        result = f"Average: {average:.2f}. Passed!"
+
+    else:
+        result = f"Average: {average:.2f}. Failed."     
+
+    display(result, target="output1")
