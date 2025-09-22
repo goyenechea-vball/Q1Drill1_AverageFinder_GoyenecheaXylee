a928035d45ca2445fe10b9a8905a7b9bf2ae43fc
goyenechea-vball <goyenechea.xa@obmontessori.edu.ph>
2025-09-22 16:38:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ display()
 def ave(e): #e for event handler
     num1 = float(document.getElementById("input1").value)
     num2 = float(document.getElementById("input2").value)
-
+#solution
     average = (num1 + num2) / 2
 
     if average >= 75:
@@ -12,5 +12,6 @@ def ave(e): #e for event handler
 
     else:
         result = f"Average: {average:.2f}. Failed."     
+
 
     display(result, target="output1")
